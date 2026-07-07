34324652dcfa4bb69f7ec6eb73bf9798bb8c4820
Miguel Aceituno Rojo <miguel.aceituno@soltechsac.com>
2026-07-07 09:03:56 -0500

    Initial commit

---
+++ app/src/androidTest/java/unap/epis/dp2/mibolsillo/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package unap.epis.dp2.mibolsillo
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("unap.epis.dp2.mibolsillo", appContext.packageName)
+    }
+}
