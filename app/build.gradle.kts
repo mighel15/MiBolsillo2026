db0135b4cc2d9e0675b68243d244e49873fb6549
Miguel Aceituno Rojo <miguel.aceituno@soltechsac.com>
2026-07-07 09:33:42 -0500

    configuración del proyecto

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 android {
     namespace = "unap.epis.dp2.mibolsillo"
     compileSdk {
-        version = release(36) {
+        version = release(37) {
             minorApiLevel = 1
         }
     }
@@ -14,7 +14,7 @@ android {
     defaultConfig {
         applicationId = "unap.epis.dp2.mibolsillo"
         minSdk = 31
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
